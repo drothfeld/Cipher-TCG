4185466fba75dd903305ad90ac7816320fa37a70
Dylan Rothfeld <drothfel@gmail.com>
2018-01-16 10:30:48 -0800

    added base inital base assets and skill class

---
+++ Cipher TCG/Classes/Skill.swift
@@ -0,0 +1,21 @@
+//
+//  Skill.swift
+//  Cipher TCG
+//
+//  Created by Dylan Rothfeld on 1/16/18.
+//  Copyright © 2018 Dylan Rothfeld. All rights reserved.
+//
+
+import UIKit
+
+class Skill {
+    // Fields
+    var name: String
+    
+    
+    // Constructor
+    init(name: String) {
+        self.name = name
+    }
+}
+
