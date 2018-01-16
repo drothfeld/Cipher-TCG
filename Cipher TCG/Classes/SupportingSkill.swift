0d26b8a7456eb0a7e8ee4ab7a3404ab031060427
Dylan Rothfeld <drothfel@gmail.com>
2018-01-16 13:27:26 -0800

    supporting skill class and inital constants

---
+++ Cipher TCG/Classes/SupportingSkill.swift
@@ -0,0 +1,21 @@
+//
+//  SupportingSkill.swift
+//  Cipher TCG
+//
+//  Created by Dylan Rothfeld on 1/16/18.
+//  Copyright © 2018 Dylan Rothfeld. All rights reserved.
+//
+
+import UIKit
+
+class SupportingSkill {
+    // Fields
+    var type: String
+    var description: String
+    
+    // Constructor
+    init(type: String, description: String) {
+        self.type = type
+        self.description = description
+    }
+}
---
+++ Cipher TCG/Constants/SupportingSkills.swift
@@ -0,0 +1,13 @@
+//
+//  SupportingSkills.swift
+//  Cipher TCG
+//
+//  Created by Dylan Rothfeld on 1/16/18.
+//  Copyright © 2018 Dylan Rothfeld. All rights reserved.
+//
+
+import UIKit
+
+let attack_emblem = SupportingSkill(type: "Attack Emblem",
+                                    description: "Until the end of this battle, your attacking unit gains +20 attack.")
+
