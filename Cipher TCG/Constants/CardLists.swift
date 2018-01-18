75bada48cd7de603292b5f7b7bc9b1a6cdc6a942
Dylan Rothfeld <drothfel@gmail.com>
2018-01-18 10:30:37 -0800

    organizing directory

---
+++ Cipher TCG/Constants/CardLists.swift
@@ -0,0 +1,55 @@
+//
+//  CardLists.swift
+//  Cipher TCG
+//
+//  Created by Dylan Rothfeld on 1/18/18.
+//  Copyright © 2018 Dylan Rothfeld. All rights reserved.
+//
+
+import UIKit
+
+let rawCardsList: [Card] = [
+    // Series: Starter Deck 03
+    s03_002,
+    // Series: Box Set 02
+    b02_003,
+    b02_006,
+    // Series: Box Set 03
+    b03_051,
+    b03_052,
+    b03_063,
+    b03_067,
+    b03_068,
+    b03_072,
+    b03_078,
+    b03_091,
+    b03_092,
+    // Series: Box Set 06
+    b06_054,
+    b06_062,
+    b06_063,
+    // Series: Box Set 07
+    b07_051,
+    b07_053,
+    b07_056,
+    b07_058,
+    b07_064,
+    b07_068,
+    b07_086,
+    // Series: Box Set 10
+    b10_051,
+    b10_052,
+    b10_053,
+    b10_054,
+    b10_055,
+    b10_057,
+    b10_059,
+    b10_060,
+    b10_061,
+    b10_064,
+    b10_065,
+    b10_068,
+    b10_076,
+    b10_081,
+    b10_086
+]
--- Cipher TCG/View Controllers/CardListViewController.swift
+++ Cipher TCG/View Controllers/CardListViewController.swift
@@ -15,30 +15,77 @@ class CardListViewController: UIViewController, UITableViewDataSource, UITableVi
     @IBOutlet weak var CardListSearchBar: UISearchBar!
     
     // Defined Values
+    var sortedRawCardList: [Card] = []
+    var filteredRawCardList: [Card] = []
     var isSearching = false
-    var testList: [String] = ["one", "two", "three", "four", "five"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         refreshTable()
+        searchBarSetup()
     }
     
     // Number of Rows
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return testList.count
+        if (isSearching) {
+            return filteredRawCardList.count
+        }
+        return rawCardsList.count
     }
     
     // Cell Data
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath)
-        cell.textLabel?.text = testList[indexPath.item]
+        if (isSearching) {
+            cell.textLabel?.text = filteredRawCardList[indexPath.item].name
+        } else {
+            cell.textLabel?.text = sortedRawCardList[indexPath.item].name
+        }
         return cell
+    }
+    
+    // Sort list of cards alphabetically by name
+    func sortCardsAlphabetically(unsortedList: Array<Card>) -> Array<Card> {
+        return unsortedList.sorted { $0.name < $1.name }
     }
     
     // Table Refresh
     func refreshTable() {
+        sortedRawCardList = sortCardsAlphabetically(unsortedList: rawCardsList)
         self.CardListTableView.reloadData()
     }
+    
+    // Search Bar Setup
+    func searchBarSetup() {
+        CardListSearchBar.delegate = self
+        CardListSearchBar.returnKeyType = UIReturnKeyType.done
+    }
+    
+    // Search Bar Functionality
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if (CardListSearchBar.text == nil || CardListSearchBar.text == "") {
+            isSearching = false
+            view.endEditing(true)
+        } else {
+            isSearching = true
+            filteredRawCardList = sortedRawCardList.filter( {$0.name.lowercased().contains(CardListSearchBar.text!.lowercased())} )
+        }
+        CardListTableView.reloadData()
+    }
+    
+    // Preparing Ingredient to get details on
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if let indexPath = CardListTableView.indexPathForSelectedRow {
+//            let card: Card
+//            if isSearching {
+//                card = filteredRawCardList[indexPath.row]
+//            } else {
+//                card = sortedRawCardList[indexPath.row]
+//            }
+//            let controller = segue.destination as! IngredientViewController
+//            controller.detailIngredient = card
+//        }
+//    }
     
     // Hiding status bar
     override var prefersStatusBarHidden: Bool {
