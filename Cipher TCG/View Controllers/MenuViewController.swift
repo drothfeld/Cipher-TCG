37c59758fe172f02ff12a68c14f78dc2b7439165
Dylan Rothfeld <drothfel@gmail.com>
2018-01-22 10:17:31 -0800

    new menu view controller

---
+++ Cipher TCG/View Controllers/MenuViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MenuViewController.swift
+//  Cipher TCG
+//
+//  Created by Dylan Rothfeld on 1/22/18.
+//  Copyright © 2018 Dylan Rothfeld. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    // Hiding status bar
+        override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
