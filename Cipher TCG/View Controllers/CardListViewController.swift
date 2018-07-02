173feb04960e17c2e8ad8105ef1963ee0fa34eed
Dylan Rothfeld <drothfel@gmail.com>
2018-07-02 11:43:04 -0700

    can now sort cards by insignia/color using uibuttons on the card list view

--- Cipher TCG/View Controllers/CardListViewController.swift
+++ Cipher TCG/View Controllers/CardListViewController.swift
@@ -15,6 +15,14 @@ class CardListViewController: UIViewController, UITableViewDataSource, UITableVi
     @IBOutlet weak var CardListTableView: UITableView!
     @IBOutlet weak var CardListSearchBar: UISearchBar!
     @IBOutlet weak var CardListViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var RedFilterButton: UIButton!
+    @IBOutlet weak var BlueFilterButton: UIButton!
+    @IBOutlet weak var WhiteFilterButton: UIButton!
+    @IBOutlet weak var BlackFilterButton: UIButton!
+    @IBOutlet weak var GreenFilterButton: UIButton!
+    @IBOutlet weak var PurpleFilterButton: UIButton!
+    @IBOutlet weak var YellowFilterButton: UIButton!
+    @IBOutlet weak var ColorlessFilterButton: UIButton!
     
     // Defined Values
     var sortedRawCardList: [Card] = []
@@ -22,6 +30,7 @@ class CardListViewController: UIViewController, UITableViewDataSource, UITableVi
     var isSearching = false
     let screenSize: CGRect = UIScreen.main.bounds
     let storyboardDeviceHeight: CGFloat = 667
+    var colorFilterButtons: [UIButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -104,6 +113,7 @@ class CardListViewController: UIViewController, UITableViewDataSource, UITableVi
             isSearching = false
             view.endEditing(true)
         } else {
+            deactivateAllFilters()
             isSearching = true
             filteredRawCardList = sortedRawCardList.filter( {$0.name.lowercased().contains(CardListSearchBar.text!.lowercased())} )
         }
@@ -128,6 +138,7 @@ class CardListViewController: UIViewController, UITableViewDataSource, UITableVi
     func interfaceSetup() {
         HeaderView.dropShadow()
         CardListViewHeightConstraint.constant += screenSize.height - storyboardDeviceHeight
+        colorFilterButtons = [RedFilterButton, BlueFilterButton, WhiteFilterButton, BlackFilterButton, GreenFilterButton, PurpleFilterButton, YellowFilterButton, ColorlessFilterButton]
     }
     
     // Hide keyboard activated from search bar
@@ -139,6 +150,141 @@ class CardListViewController: UIViewController, UITableViewDataSource, UITableVi
     override var prefersStatusBarHidden: Bool {
         return true
     }
-
+    
+    // Deactivate all filter buttons
+    // Optional Param: Don't deactivate specified button
+    func deactivateAllFilters(keepButton: UIButton? = nil) {
+        for button in colorFilterButtons {
+            if (button != keepButton) {
+                deactivateFilterButton(button: button)
+            }
+        }
+    }
+    
+    // Deactivate filter button
+    func deactivateFilterButton(button: UIButton) {
+        button.alpha = 0.5
+        isSearching = false
+    }
+    
+    // Activate filter button
+    func activateFilterButton(button: UIButton) {
+        deactivateAllFilters(keepButton: button)
+        button.alpha = 1.0
+        CardListSearchBar.text = nil
+        isSearching = true
+    }
+    
+    // Filter red cards
+    @IBAction func filterRedCardsButtonPressed(_ sender: Any) {
+        let buttonPressed = sender as! UIButton
+        // Deactivate filter
+        if (buttonPressed.alpha == 1.0) {
+            deactivateFilterButton(button: buttonPressed)
+        // Activate filter
+        } else {
+            activateFilterButton(button: buttonPressed)
+            filteredRawCardList = sortedRawCardList.filter({$0.insignia.name.contains(blade_of_light.name)})
+        }
+        CardListTableView.reloadData()
+    }
+    
+    // Filter blue cards
+    @IBAction func filterBlueCardsButtonPressed(_ sender: Any) {
+        let buttonPressed = sender as! UIButton
+        // Deactivate filter
+        if (buttonPressed.alpha == 1.0) {
+            deactivateFilterButton(button: buttonPressed)
+            // Activate filter
+        } else {
+            activateFilterButton(button: buttonPressed)
+            filteredRawCardList = sortedRawCardList.filter({$0.insignia.name.contains(mark_of_naga.name)})
+        }
+        CardListTableView.reloadData()
+    }
+    
+    // Filter white cards
+    @IBAction func filterWhiteCardsButtonPressed(_ sender: Any) {
+        let buttonPressed = sender as! UIButton
+        // Deactivate filter
+        if (buttonPressed.alpha == 1.0) {
+            deactivateFilterButton(button: buttonPressed)
+            // Activate filter
+        } else {
+            activateFilterButton(button: buttonPressed)
+            filteredRawCardList = sortedRawCardList.filter({$0.insignia.name.contains(hoshido.name)})
+        }
+        CardListTableView.reloadData()
+    }
+    
+    // Filter black cards
+    @IBAction func filterBlackCardsButtonPressed(_ sender: Any) {
+        let buttonPressed = sender as! UIButton
+        // Deactivate filter
+        if (buttonPressed.alpha == 1.0) {
+            deactivateFilterButton(button: buttonPressed)
+            // Activate filter
+        } else {
+            activateFilterButton(button: buttonPressed)
+            filteredRawCardList = sortedRawCardList.filter({$0.insignia.name.contains(nohr.name)})
+        }
+        CardListTableView.reloadData()
+    }
+    
+    // Filter green cards
+    @IBAction func filterGreenCardsButtonPressed(_ sender: Any) {
+        let buttonPressed = sender as! UIButton
+        // Deactivate filter
+        if (buttonPressed.alpha == 1.0) {
+            deactivateFilterButton(button: buttonPressed)
+            // Activate filter
+        } else {
+            activateFilterButton(button: buttonPressed)
+            filteredRawCardList = sortedRawCardList.filter({$0.insignia.name.contains(medallion.name)})
+        }
+        CardListTableView.reloadData()
+    }
+    
+    // Filter purple cards
+    @IBAction func filterPurpleCardsButtonPressed(_ sender: Any) {
+        let buttonPressed = sender as! UIButton
+        // Deactivate filter
+        if (buttonPressed.alpha == 1.0) {
+            deactivateFilterButton(button: buttonPressed)
+            // Activate filter
+        } else {
+            activateFilterButton(button: buttonPressed)
+            filteredRawCardList = sortedRawCardList.filter({$0.insignia.name.contains(divine_weapons.name)})
+        }
+        CardListTableView.reloadData()
+    }
+    
+    // Filter yellow cards
+    @IBAction func filterYellowCardsButtonPressed(_ sender: Any) {
+        let buttonPressed = sender as! UIButton
+        // Deactivate filter
+        if (buttonPressed.alpha == 1.0) {
+            deactivateFilterButton(button: buttonPressed)
+            // Activate filter
+        } else {
+            activateFilterButton(button: buttonPressed)
+            filteredRawCardList = sortedRawCardList.filter({$0.insignia.name.contains(holy_war_flag.name)})
+        }
+        CardListTableView.reloadData()
+    }
+    
+    // Filter colorless cards
+    @IBAction func filterColorlessCardsButtonPressed(_ sender: Any) {
+        let buttonPressed = sender as! UIButton
+        // Deactivate filter
+        if (buttonPressed.alpha == 1.0) {
+            deactivateFilterButton(button: buttonPressed)
+            // Activate filter
+        } else {
+            activateFilterButton(button: buttonPressed)
+            filteredRawCardList = sortedRawCardList.filter({$0.insignia.name.contains(colorless.name)})
+        }
+        CardListTableView.reloadData()
+    }
 }
 
