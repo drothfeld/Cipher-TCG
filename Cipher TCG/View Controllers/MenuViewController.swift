75610a75ab665f83af2e3a67e157f91e0ca9631f
Dylan Rothfeld <drothfel@gmail.com>
2018-09-17 19:41:44 -0700

    xcode 10 update

--- Cipher TCG/View Controllers/MenuViewController.swift
+++ Cipher TCG/View Controllers/MenuViewController.swift
@@ -12,11 +12,16 @@ class MenuViewController: UIViewController {
     
     // Sends the User to Emblem Wiki
     @IBAction func linkToEmblemWiki(_ sender: Any) {
-        UIApplication.shared.open(URL(string: "https://serenesforest.net/wiki/index.php/Fire_Emblem_TCG")!, options: [:], completionHandler: nil)
+        UIApplication.shared.open(URL(string: "https://serenesforest.net/wiki/index.php/Fire_Emblem_TCG")!, options: convertToUIApplicationOpenExternalURLOptionsKeyDictionary([:]), completionHandler: nil)
     }
     
     // Hiding status bar
         override var prefersStatusBarHidden: Bool {
         return true
     }
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertToUIApplicationOpenExternalURLOptionsKeyDictionary(_ input: [String: Any]) -> [UIApplication.OpenExternalURLOptionsKey: Any] {
+	return Dictionary(uniqueKeysWithValues: input.map { key, value in (UIApplication.OpenExternalURLOptionsKey(rawValue: key), value)})
 }
