2a4e189b3253f476b9762496a76e34a38b9c87df
Dylan Rothfeld <drothfel@gmail.com>
2019-03-05 11:28:20 -0800

    stub view controller for Illustrators screen

---
+++ Cipher TCG/View Controllers/IllustratorsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  IllustratorsViewController.swift
+//  Cipher TCG
+//
+//  Created by Dylan Rothfeld on 3/5/19.
+//  Copyright © 2019 Dylan Rothfeld. All rights reserved.
+//
+
+import UIKit
+
+class IllustratorsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
