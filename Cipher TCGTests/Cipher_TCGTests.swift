e3396b8def7d15ae3bbd201a06dcdacf0c1b2132
Dylan Rothfeld <drothfel@gmail.com>
2019-04-03 11:00:55 -0700

    new unit tests for Card model functions

--- Cipher TCGTests/Cipher_TCGTests.swift
+++ Cipher TCGTests/Cipher_TCGTests.swift
@@ -31,6 +31,36 @@ class Cipher_TCGTests: XCTestCase {
         // Then
         XCTAssertFalse(result)
     }
+    
+    // UNIT TEST:
+    // Tests the functionality of removing a cipher
+    // card from the users favorites list.
+    func testRemoveFromFavoritesList() {
+        // Given
+        let card_a: Card = Card(series: "B00-000N", name: "Roy: Our Boy", rarity: normal, insignia: divine_weapons, attack: 70, support: 20, illustrator: mayo, supportingSkill: none, skills: [legendary_divine_knight, dragonslaying_radiance], cardImageName: "B00-000N")
+        favorite_cards.append(card_a)
+        
+        // When
+        card_a.removeFromFavoritesList()
+        
+        // Then
+        XCTAssertFalse(favorite_cards.contains(card_a))
+    }
+    
+    // UNIT TEST:
+    // Tests the functionality of checking whether
+    // one cipher card is more rare than another
+    func testIsMoreRareThan() {
+        // Given
+        let card_a: Card = Card(series: "B00-000N", name: "Roy: Our Boy", rarity: normal, insignia: divine_weapons, attack: 70, support: 20, illustrator: mayo, supportingSkill: none, skills: [legendary_divine_knight, dragonslaying_radiance], cardImageName: "B00-000N")
+        let card_b: Card = Card(series: "B00-001HN", name: "Marth: The Dashing", rarity: high_normal, insignia: mark_of_naga, attack: 70, support: 20, illustrator: mayo, supportingSkill: none, skills: [immortal_falchion, light_road_to_the_invisible_kingdom], cardImageName: "B00-01HN")
+        
+        // When
+        let result = card_a.isMoreRareThan(card: card_b)
+        
+        // Then
+        XCTAssertFalse(result)
+    }
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
